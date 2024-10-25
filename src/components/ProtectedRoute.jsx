b6c3a2e1faaa4504fec2cef98ff5e7e1b8fec960
Prayash007 <Prayash007@github.com>
2024-10-25 11:52:58 +0530

    alert added

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,17 @@
 // src/components/ProtectedRoute.js
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useFirebase } from '../context/FirebaseContext';
 
 const ProtectedRoute = () => {
     const { userId } = useFirebase(); // Access userId from context
+
+    useEffect(() => {
+        if (userId === 'guest') {
+            alert("Please log in to access this page.");
+        }
+    }, [userId]);
 
     // If user is not authenticated, redirect to home
     if (userId === 'guest') {
